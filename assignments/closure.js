a02f54764d15eaf194819b6ef20f934c4fbe3574
Victor Sanchez <v08311996@gmail.com>
2019-09-09 17:42:11 -0400

    Updated some code

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,13 +3,31 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+function greeting() {
+  let hello = "Nice to meet you";
 
+
+  function fullGreeting(first, last) {
+      return `${hello} ${first} ${last}!`;
+  }
+  return fullGreeting;
+}
+
+const sayHello = greeting();
+console.log(sayHello("Jess", "H"));
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
+  let count = 0;
+  const plusOne = function() {
+      return ++count;
+  }
+  return plusOne;
+
+
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
   // 2- Declare a function `counter`. It should increment and return `count`.
@@ -17,6 +35,10 @@ const counterMaker = () => {
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
 };
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
